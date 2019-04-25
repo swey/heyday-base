0e1cb7085eae3076749c8e4c9814bedf2a7d37e9
Sebastian Weyrauch <git@dev-lab.de>
2019-04-25 15:51:15 +0200

    feat: added refactored version of font loader

---
+++ js/services/FontLoader.js
@@ -0,0 +1,63 @@
+import FontFaceObserver from 'fontfaceobserver/fontfaceobserver';
+
+export const events = {
+	fontsLoaded: 'fontsLoaded.FontLoader'
+};
+
+/**
+ * FontLoader
+ */
+export default class FontLoader {
+	constructor() {
+		this._fontGroups = [];
+	}
+
+	addGroup(className, list) {
+		this._fontGroups.push(FontLoader._loadFonts(className, list));
+	}
+
+	/**
+	 * Loads fonts with FontFaceObserver
+	 *
+	 * @public
+	 */
+	async start() {
+		if (!this._fontGroups.length) {
+			return;
+		}
+
+		await Promise.all(this._fontGroups);
+
+		// Trigger an event as soon all fonts are loaded
+		window.setTimeout(() => document.dispatchEvent(new CustomEvent(events.fontsLoaded)), 100);
+	}
+
+	/**
+	 * Loads the fonts in list, saves the load status in the
+	 * session and adds the given className to the className
+	 * of the html tag.
+	 *
+	 * @param {string} className
+	 * @param {Array.<FontFaceObserver>} list
+	 * @private
+	 */
+	static async _loadFonts(className, list) {
+		try {
+			await Promise.all(list.map(font => font.load(null, 5000)));
+
+			document.documentElement.className += ` ${className}`;
+
+			// console.log(`${className} with FontFaceObserver.`);
+
+			try {
+				sessionStorage[className] = 'true';
+			} catch (error) {
+			}
+		} catch (err) {
+			try {
+				sessionStorage[className] = 'false';
+			} catch (error) {
+			}
+		}
+	}
+}
---
+++ js/services/FontLoaderHead.js
@@ -0,0 +1,31 @@
+/**
+ * FontLoaderHead
+ *
+ * For usage in head of the pages.
+ */
+export default class FontLoaderHead {
+	/**
+	 * If the font is cached, add class right away
+	 * for immediate use.
+	 *
+	 * @private
+	 */
+	static addLoadedFontsFont(className) {
+		if (sessionStorage[className] === 'true' || FontLoaderHead._isExcludedBrowser()) {
+			document.documentElement.className += ` ${className}`;
+		}
+	}
+
+	/**
+	 * Exclude some browsers from the default font loading.
+	 *
+	 * IE14 (Edge): Loads the fonts without problems, but is not able to validate that
+	 * bold fonts were loaded (for fonts with "font-weight: normal" that works fine as well)
+	 *
+	 * @returns {boolean}
+	 * @private
+	 */
+	static _isExcludedBrowser() {
+		return /edge\/14\./i.test(navigator.userAgent);
+	}
+}
